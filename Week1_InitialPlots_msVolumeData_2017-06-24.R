59bdb0af79635fe9d3124ff53ad7c4a91d13696e
brilliantFire <rlvislaywade@outlook.com>
2017-06-24 17:31:07 -0400

    Update Week1_InitialPlots_msVolumeData_2017-06-24.R

--- Week1_InitialPlots_msVolumeData_2017-06-24.R
+++ Week1_InitialPlots_msVolumeData_2017-06-24.R
@@ -7,7 +7,8 @@
 ### This program produces two different kinds of plots of time series
 ### data consisting of the daily number of scientific manuscripts 
 ### entering an editorial workflow. The first is a basic time series
-### plot using plot.ts(). The second uses ggplot2.
+### plot using plot.ts(). The second uses ggplot2. Two additional
+### ggplot2 plots show 6 month and 1 month subsets.
 
 # Read in from .csv
 daily <- read.csv("dailyVolumeCleaned.csv", header = TRUE)
@@ -49,3 +50,47 @@ ggplot(data=daily, aes(x=date, y=mss, group=1)) +
   # Use scale_*_* functions from the scales package to set axes scales
   scale_y_continuous(limits = c(0,300), breaks=seq(0,300,100)) + 
   scale_x_date(breaks = date_breaks("months"), labels = date_format("%Y-%m-%d"))
+
+# Let's look at just the 6 months of Jul-Dec 2016
+daily_6months <- daily[130:253,]
+
+# PLOT 03: Same plot as before except just three months:
+ggplot(data=daily_6months, aes(x=date, y=mss, group=1)) +
+  geom_line(color = "forestgreen", size = 1) +
+  # Plot and title labels
+  xlab("Date") +
+  ylab("Manuscripts Received") +
+  ggtitle("Daily Manuscript Volume - 7/1/2016 to 12/31/2016") +
+  # "The Economist" magazine theme
+  theme_economist() +
+  # Here we adjust all the font sizes, angles, and justifications in a 
+  # theme() call.
+  theme(axis.text.x=element_text(size = 14, vjust = 1.0, hjust = 1.0, angle = 45),
+        axis.text.y = element_text(size = 14),
+        axis.title.x = element_text(size = 18),
+        axis.title.y = element_text(size = 18, vjust = 2),
+        axis.title = element_text(size = 24)) +
+  scale_y_continuous(limits = c(0,300), breaks=seq(0,300,100)) + 
+  scale_x_date(breaks = date_breaks("2 weeks"), labels = date_format("%Y-%m-%d"))
+
+# Now, just the month of August 2016
+daily_Aug2016 <- daily[150:172,]
+
+# PLOT 03: Same plot as before except just three months:
+ggplot(data=daily_Aug2016, aes(x=date, y=mss, group=1)) +
+  geom_line(color = "forestgreen", size = 1) +
+  # Plot and title labels
+  xlab("Date") +
+  ylab("Manuscripts Received") +
+  ggtitle("Daily Manuscript Volume - August 2016") +
+  # "The Economist" magazine theme
+  theme_economist() +
+  # Here we adjust all the font sizes, angles, and justifications in a 
+  # theme() call.
+  theme(axis.text.x=element_text(size = 14, vjust = 1.0, hjust = 1.0, angle = 45),
+        axis.text.y = element_text(size = 14),
+        axis.title.x = element_text(size = 18),
+        axis.title.y = element_text(size = 18, vjust = 2),
+        axis.title = element_text(size = 22)) +
+  scale_y_continuous(limits = c(0,300), breaks=seq(0,300,100)) + 
+  scale_x_date(breaks = date_breaks("2 days"), labels = date_format("%Y-%m-%d"))
